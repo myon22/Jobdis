f16dda59a8789febaceae19f3111d8e6a430cfe9
shoma ishizaki <uthorium232@gmail.com>
2019-12-19 01:39:46 +0900

    up1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://shrouded-river-97390.herokuapp.com'
+  host = 'https://pacific-atoll-78023.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
